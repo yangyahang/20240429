3cb793bbbc23393506532b09218760a8bbf56515
yangyahang <150992210+yangyahang@users.noreply.github.com>
2024-04-29 11:47:26 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,16 +16,19 @@ function setup() {
   radioElement.position(width/2-300,20)
   radioElement.option("圓點")
   radioElement.option("方塊")
+  radioElement.option("亮度")
+  radioElement.option("炫紅")
+  radioElement.option("文字")
   radioElement.style("font-size","25px")
   radioElement.style("color","#fef9ef")
   radioElement.style("background","#14213d")
-  radioElement.style("width","16%")
+  radioElement.style("width","50%")
   radioElement.style("border","2px solid #fca3113")
   radioElement.style("border-radius","25px")
 }
 
 function draw() {
-  background(220);
+  background(0);
   noStroke()
   span=5+map(mouseX,0,width,0,20)
   push()
@@ -41,7 +44,28 @@ function draw() {
       if(radioElement.value()=="方塊"){
         rect(x,y,span)  
       }
-     }
+      if(radioElement.value()=="亮度"){
+        bk=(pixel[0]+pixel[1]+pixel[2])/3
+        fill(bk)
+        ellipse(x,y,span*map(bk,0,255,0.8,1.2)) 
+      }
+      if(radioElement.value()=="炫紅"){
+        colorMode(HSB)
+        fill(pixel[0],80,80)
+        rectMode(CENTER)
+        rect(x,y,span)
+        fill(0)
+        ellipse(x,y,5)
+      }
+      if(radioElement.value()=="炫紅"){
+        const density = 'Ñ@#W$9876543210?!abc;:+=-,._ ';
+        let txt = "一二三四五田雷電龕龘"
+        bk=(pixel[0]+pixel[1]+pixel[2])/3
+        let bkId = int(map(bk, 0, 255, 9, 0))
+        textSize(span)
+        text(txt[bkId],x,y) 
+      }
     }
-  pop()
+   }
+   pop()
 }
