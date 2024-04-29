1b6ddcda4c77934b1bab216ed053a8ce52ff701e
yangyahang <150992210+yangyahang@users.noreply.github.com>
2024-04-29 10:52:01 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,10 +10,22 @@ function setup() {
   captureGraphics.translate(capture_width,0)
   captureGraphics.scale(-1,1)
   capture.hide()
+  //============================================================以上為螢幕設定
+  radioElement=createRadio()
+  radioElement.position(width*4/5,20)
+  radioElement.option("圓點")
+  radioElement.option("方塊")
+  radioElement.style("color","#fca311")
+  radioElement.style("background","#14213d")
+  radioElement.style("width","7%")
+  radioElement.style("border","3px solid #e5e5e5")
+  radioElement.style("border-radius","25px")
 }
 
 function draw() {
   background(220);
+  noStroke()
+  span=5+map(mouseX,0,width,0,20)
   push()
    translate(width/2-capture_width/2,height/2-capture_height/2)
    captureGraphics.image(capture,0,0)
@@ -22,7 +34,6 @@ function draw() {
       var pixel = captureGraphics.get(x,y)
       fill(pixel)
       rect(x,y,span)
-      noStroke()
     }
    }
   pop()
